78841e42c908f41dda47ba7b9a18334f4cda74b1
ahme2001 <ahmedaboeleid30@gmail.com>
2023-01-30 21:19:09 +0200

    Time: 0  ms (100%), Space: 42.8 MB (79.44%)

---
+++ leetcode/src/main/java/BinarySearch704.java
@@ -0,0 +1,18 @@
+public class BinarySearch704 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = (left+right)/2;
+        while (left <= right){
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+            mid = (left+right)/2;
+        }
+        return -1;
+    }
+}
