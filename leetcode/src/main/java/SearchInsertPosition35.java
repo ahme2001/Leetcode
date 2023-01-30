40e0f4e52a3838e05008626ecfd9b421f012e400
ahme2001 <ahmedaboeleid30@gmail.com>
2023-01-30 22:09:22 +0200

    Time: 0  ms (100%), Space: 41.5 MB (95.88%)

---
+++ leetcode/src/main/java/SearchInsertPosition35.java
@@ -0,0 +1,19 @@
+public class SearchInsertPosition35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = (left+right)/2;
+        while (left <= right){
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+            mid = (left+right)/2;
+        }
+        return left;
+    }
+}
