a6c140db7d565b29a1ea3eaa5b62026b42b719f9
ahme2001 <ahmedaboeleid30@gmail.com>
2023-02-01 18:32:45 +0200

    Time: 7  ms (95.28%), Space: 42.7 MB (86.29%)

---
+++ leetcode/src/main/java/ValidPalindromeII680.java
@@ -0,0 +1,20 @@
+public class ValidPalindromeII680 {
+    public boolean validPalindrome(String s) {
+        for(int i=0,j=s.length()-1;i<j;i++,j--) {
+            if (!(s.charAt(i) == s.charAt(j))) {
+                if (validPalindromeII(s, i + 1, j) || validPalindromeII(s, i, j - 1))
+                    break;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+    public boolean validPalindromeII(String s,int st,int end) {
+        for(int i=st,j=end;i<j;i++,j--){
+            if (!(s.charAt(i) == s.charAt(j)))
+                return false;
+        }
+        return true;
+    }
+}
