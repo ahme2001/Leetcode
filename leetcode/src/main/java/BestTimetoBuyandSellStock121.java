57125bb668aeb18c38103b22439aa6625440f972
ahme2001 <ahmedaboeleid30@gmail.com>
2023-02-14 22:56:36 +0200

    Time: 2 ms (64.69%), Space: 58.8  MB (84.23%)

--- leetcode/src/main/java/BestTimetoBuyandSellStock121.java
+++ leetcode/src/main/java/BestTimetoBuyandSellStock121.java
@@ -13,4 +13,19 @@ public class BestTimetoBuyandSellStock121 {
         }
         return max;
     }
+
+    // Optimization to function above
+    public int maxProfitO(int[] prices) {
+        if(prices.length <= 1) return 0;
+        int max = prices[0],min = prices[0],res=0;
+        for(int num:prices){
+            if (num < min){
+                res = Math.max(res,max-min);
+                min = num;max=num;
+            }else
+                max = Math.max(max,num);
+        }
+        res = Math.max(res,max-min);
+        return res;
+    }
 }
