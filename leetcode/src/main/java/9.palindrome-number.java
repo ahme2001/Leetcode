6104d99b50db4a58bfffac9a150d303f88cf52a0
ahme2001 <ahmedaboeleid30@gmail.com>
2023-05-15 22:47:10 +0300

    "Time: 9 ms (99.38%), Space: 42 MB (60.72%)"

---
+++ leetcode/src/main/java/9.palindrome-number.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int temp = x;
+        int rev = 0;
+        while(temp>0){
+            rev = rev*10 + temp%10;
+            temp = temp/10;
+        }
+        if(rev == x)
+            return true;
+        return false;
+    }
+}
+// @lc code=end
+
