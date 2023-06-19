95be092b5497547b820f57c8909b7e33f7e3d8d6
ahme2001 <ahmedaboeleid30@gmail.com>
2023-06-19 19:27:02 +0300

    Time: 7 ms (6.2%), Space: 44.4 MB (51.80%)

--- leetcode/src/main/java/AddTwoNumbers2.java
+++ leetcode/src/main/java/AddTwoNumbers2.java
@@ -2,49 +2,46 @@ public class AddTwoNumbers2 {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode();
-        ListNode res = head;
         int remain = 0;
+        ListNode res = head;
+        int value;
         while (l1 != null  && l2 != null){
-            int val = l1.val + l2.val + remain;
-            if(val > 9 ) {
-                remain = val % 9;
-                head.val = 9;
-            }else {
-                remain = 0;
-                head.val = val;
+            value = l1.val + l2.val + remain;
+            head.val = value % 10;
+            remain = value / 10;
+            if(l1.next != null  || l2.next != null){
+                head.next = new ListNode();
+                head = head.next;
             }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while(l1 != null){
+            value = l1.val + remain;
+            head.val = value % 10;
+            remain = value / 10;
+            if(l1.next != null){
+                head.next = new ListNode();
+                head = head.next;
+            }
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            value = l2.val + remain;
+            head.val = value % 10;
+            remain = value / 10;
+            if(l2.next != null){
+                head.next = new ListNode();
+                head = head.next;
+            }
+            l2 = l2.next;
+        }
+        if(remain > 0){
             head.next = new ListNode();
             head = head.next;
-            l1 = l1.next;
-            l2 = l2.next;
+            head.val = remain;
         }
-        int val;
-        while (l1 != null){
-            val = l1.val + remain;
-            if(val > 9 ) {
-                remain = val % 9;
-                head = new ListNode(9,new ListNode());
-            }else {
-                remain = 0;
-                head = new ListNode(val,new ListNode());
-
-            }
-            head = head.next;
-            l1 = l1.next;
-        }
-        while (l2 != null){
-            val = l2.val + remain;
-            if(val > 9 ) {
-                remain = val % 9;
-                head = new ListNode(9,new ListNode());
-            }else {
-                remain = 0;
-                head = new ListNode(val,new ListNode());
-
-            }
-            l2 = l2.next;
-            head = head.next;
-        }
+        System.out.println(remain);
         return res;
     }
 
