59f894030a0230f7f93822da56b4b22f45b0b11b
ahme2001 <ahmedaboeleid30@gmail.com>
2023-06-24 15:19:31 +0300

    Time: 1 ms (95%), Space: 43.4 MB (26.80%)

---
+++ leetcode/src/main/java/generate-parentheses.java
@@ -0,0 +1,20 @@
+class generate-parentheses {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        generate("",n,n);
+        return res;
+    }
+    private void generate(String sb,int open,int close){
+        if(open > close)    return;
+        if(open > 0 && close > 0){
+            generate(sb + "(",open - 1,close);
+        }
+        if(close > 0){
+            generate(sb + ")",open,close-1);
+        }
+        if(open==0 & close == 0)
+            res.add(sb);
+        return;
+    }
+    
+}
