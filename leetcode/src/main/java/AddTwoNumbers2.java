28a2e32d42d92e084a0b04ad508c9b4460dd16e7
ahme2001 <ahmedaboeleid30@gmail.com>
2023-07-29 17:20:35 +0300

    Time: 1 ms (100%), Space: 43 MB (89.51%%)

--- leetcode/src/main/java/AddTwoNumbers2.java
+++ leetcode/src/main/java/AddTwoNumbers2.java
@@ -1,48 +1,27 @@
 public class AddTwoNumbers2 {
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode();
         int remain = 0;
         ListNode res = head;
-        int value;
-        while (l1 != null  && l2 != null){
-            value = l1.val + l2.val + remain;
+        int value,value1,value2;
+        while (l1 != null  || l2 != null){
+            value1 = l1 == null?0:l1.val;
+            value2 = l2 == null?0:l2.val;
+            value = value1 + value2 + remain;
             head.val = value % 10;
             remain = value / 10;
-            if(l1.next != null  || l2.next != null){
+            l1 = l1 == null?null:l1.next;
+            l2 = l2 == null?null:l2.next;
+            if(l1 != null ||  l2 != null){
                 head.next = new ListNode();
                 head = head.next;
             }
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        while(l1 != null){
-            value = l1.val + remain;
-            head.val = value % 10;
-            remain = value / 10;
-            if(l1.next != null){
-                head.next = new ListNode();
-                head = head.next;
-            }
-            l1 = l1.next;
-        }
-        while(l2 != null){
-            value = l2.val + remain;
-            head.val = value % 10;
-            remain = value / 10;
-            if(l2.next != null){
-                head.next = new ListNode();
-                head = head.next;
-            }
-            l2 = l2.next;
         }
         if(remain > 0){
             head.next = new ListNode();
             head = head.next;
             head.val = remain;
         }
-        System.out.println(remain);
         return res;
     }
-
 }
